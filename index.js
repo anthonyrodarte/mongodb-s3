575562dc1d8e2ce3b6bbb2a3a93f48694be04375
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-07-18 08:54:30 -0700

    Removed commented out code.

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ require('dotenv/config')
 var s3 = require('s3')
 
 var client = s3.createClient({
-  maxAsyncS3: 20,     // this is the default
-  s3RetryCount: 3,    // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
+  maxAsyncS3: 20,
+  s3RetryCount: 3,
+  s3RetryDelay: 1000,
+  multipartUploadThreshold: 20971520,
+  multipartUploadSize: 15728640,
   s3Options: {
     accessKeyId: process.env.AKID,
     secretAccessKey: process.env.SECRET,
-    // any other options are passed to new AWS.S3()
   },
 })
 
