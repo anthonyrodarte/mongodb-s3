e80fd83344107b8646bf08328ff3c2bbc5e39b00
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-07-18 12:58:40 -0700

    Store actual Key in DB only after file has been succesfully uploaded to s3. Closes #2

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ const s3 = require('s3')
 const mongodb = require('mongodb')
 const { MongoClient } = mongodb
 
-MongoClient
-  .connect('mongodb://' + process.env.MONGOUSER + ':' + process.env.MONGOPW + '@ds141641.mlab.com:41641/s3-ids', {useNewUrlParser: true})
-  .then(client => {
-  const db = client.db('s3-ids')
-  const collection = db.collection('keys')
-  const testKey = {
-    id: uuid(),
-    key: 'this is a test key'
-  }
-  return collection
-  .insertOne(testKey)
-  .then(() => client.close())
-  })
-  .catch(err => {
-    console.error(err)
-    process.exit(1)
-  })
-
 const s3client = s3.createClient({
   maxAsyncS3: 20,
   s3RetryCount: 3,
@@ -42,14 +24,33 @@ const params = {
     Key: "randomscreenshot1",
   },
 }
-const uploader = s3client.uploadFile(params)
-uploader.on('error', function(err) {
-  console.error("unable to upload:", err.stack)
-});
-uploader.on('progress', function() {
-  console.log("progress", uploader.progressMd5Amount,
-            uploader.progressAmount, uploader.progressTotal)
-});
-uploader.on('end', function() {
-  console.log("upload complete")
-})
+
+MongoClient
+  .connect('mongodb://' + process.env.MONGOUSER + ':' + process.env.MONGOPW + '@ds141641.mlab.com:41641/s3-ids', {useNewUrlParser: true})
+  .then(client => {
+  const db = client.db('s3-ids')
+  const collection = db.collection('keys')
+  const uploader = s3client.uploadFile(params)
+  uploader.on('error', function(err) {
+    console.error("unable to upload:", err.stack)
+  })
+  uploader.on('progress', function() {
+    console.log("progress", uploader.progressMd5Amount,
+              uploader.progressAmount, uploader.progressTotal)
+  })
+  uploader.on('end', function() {
+    console.log("upload complete")
+    return params.s3Params.Key
+  })
+  const realKey = {
+    id: uuid(),
+    key: params.s3Params.Key
+  }
+  return collection
+  .insertOne(realKey)
+  .then(() => client.close())
+  })
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
